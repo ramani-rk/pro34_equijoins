de11cece324dac5af6ff9df478593323c3bc10db
ramani-rk <ramanikanthsasi@gmail.com>
2024-01-31 10:59:57 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -6,5 +6,13 @@ from app.models import *
 
 def equijoin(request):
     EMO=Emp.objects.select_related('deptno').all()
+    EMO=Emp.objects.select_related('deptno').filter(hiredate__year=2024)
+    EMO=Emp.objects.select_related('deptno').filter(mgr__isnull=True)
+    EMO=Emp.objects.select_related('deptno').all()
+    EMO=Emp.objects.select_related('deptno').filter(deptno__dloc='Bengaluru')
+    EMO=Emp.objects.select_related('deptno').filter(sal__gt=20000)
+    EMO=Emp.objects.select_related('deptno').filter(comm__isnull=True)
+    EMO=Emp.objects.select_related('deptno').filter(comm__isnull=False)
+    EMO=Emp.objects.select_related('deptno').all()
     d={'EMO' : EMO}
     return render (request,'equijoin.html',d)
